6f3f845c1ee2152e34ab5de926ad98ea58d4aec2
anshikamittal7 <anshikamittal34@gmail.com>
2023-08-29 00:54:40 +0530

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
 import { useSelector } from "react-redux";
 
+import logo from "../assets/logo.jpg";
+
 const Header = () => {
   const {cartItems} = useSelector(state => state.cart);
   return (
     <nav>
-      <h1>LOGO</h1>
+      <img src={logo} alt="LOGO"/>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/cart"}>
